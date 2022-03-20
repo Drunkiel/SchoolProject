f70173072396bf103f864cd2396d33f8af1a71b9
Drunkiel <drunkielfel@gmail.com>
2022-03-20 15:38:10 +0100

    Small changes

--- Assets/Scripts/ScoreMeter.cs
+++ Assets/Scripts/ScoreMeter.cs
@@ -9,12 +9,6 @@ public class ScoreMeter : MonoBehaviour
 
     public Text ScoreText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
