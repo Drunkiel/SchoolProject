0b9fc24894c18b4dd80c08b4e7c8631800341008
Drunkiel <drunkielfel@gmail.com>
2022-03-20 12:12:33 +0100

    Added ScoreMeter

---
+++ Assets/Scripts/ScoreMeter.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreMeter : MonoBehaviour
+{
+    public float Score;
+    public float Record;
+    public bool isGameRunning;
+
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = Score.ToString();
+
+        if(isGameRunning){
+            StartScore();
+        }   else{
+            StopScore();
+        }
+    }
+
+    void StartScore(){
+        Score += Time.deltaTime;
+        Score = Mathf.Round(Score * 100) / 100;
+        print(Score);
+    }
+
+    void StopScore(){
+        if(Record < Score){
+            Record = Score;
+        }
+        Score = 0;
+    }
+}
